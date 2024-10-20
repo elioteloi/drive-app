f2d0f29e8e1482c7ff4a17ac0a130bdc4dddc9ea
Eliot Eloi <eliot.eloi22@gmail.com>
2024-10-20 13:54:59 -0300

    main file for the app

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react'
+import AppNavigation from './src/navigation/AppNavigation';
+
+const App = () => {
+
+  return (
+    <>
+      <AppNavigation/>
+    </>
+  );
+};
+
+export default App;
